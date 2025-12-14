9acd4b67abbd36899d9bf0d4958433e9c2fdfc12
Manh Cuong Do <domanhcuong.cdo@gmail.com>
2025-12-14 09:48:02 -0600

    add exercies

---
+++ python/189/Solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def rotate(self, nums, k):
+        n = len(nums)
+        k = k % n
+
+        # helper reverse nums[l:r]
+        def reverse(l, r):
+            while l < r:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+                r -= 1
+
+        # reverse whole array
+        reverse(0, n - 1)
+
+        # reverse first k elements
+        reverse(0, k - 1)
+
+        # reverse remaining n - k elements
+        reverse(k, n - 1)
+
